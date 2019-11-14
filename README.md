917b53892f94da6c6ba1d887e1c7ede716c760e7
Nathaniel Rand <nathanielrand@Nathaniels-MacBook-Air.local>
2019-11-14 14:28:24 -0500

    First commit of version 0.1.0

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # GoALert
 
-# A SMS alert system for Forex RSI levels. 
+# A SMS alert system for Forex Quotes. 
 ## Messages sent via Twilio API. FX data feed via IEX Cloud API.
 
 ### TODO:
-#### - Connect/Send Twilio message
-#### - Connect/Receive IEX Cloud RSI level
-#### - Build logic for RSI breaks and SMS alerts
+#### - ~~Connect/Send Twilio message~~
+#### - ~~Connect/Receive IEX Cloud Quotes~~
+#### - ~~Send IEX Quotes via Twilio SMS~~
+#### - Add RSI level indicator
 #### - Add support for all FX majors
-#### - Add volume indicator
+#### - Add volume level indicator
 
 Twilio Reference: http://blog.magmalabs.io/2015/01/06/sending-sms-messages-using-twilio-and-golang.html
 IEX Cloud Reference: n/a
--- main.go
+++ main.go
@@ -1,29 +1,85 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/subosito/twilio"
 )
 
-var (
-	AccountSid = "AC35ddabb983353c74dc8f9149db26a5f5"
-	AuthToken  = "bacba053badfeb5ecce41afcbef528fa"
-	From       = "+18437906751"
-	To         = "+18434696850"
-)
+// init is invoked before main()
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
-	// Initialize Twilio client
-	c := twilio.NewClient(AccountSid, AuthToken, nil)
 
-	// Send SMS
-	// Add FX info to body
-	params := twilio.MessageParams{
-		Body: "goAlertFX",
+	// IEX Cloud API Reference
+	// GET https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols={symbol}&token={YOUR_TOKEN}
+
+	// GET full json response from IEX API forex quotes.
+	response, err := http.Get("https://sandbox.iexapis.com/stable/fx/latest?symbols=AUDUSD,USDJPY,EURUSD,USDZAR&token=Tsk_4d1510e26f304e95bcb49028549414a9")
+	if err != nil {
+		log.Fatalf("Error while calling IEX Cloud API via http.GET %v\n", err)
+		return
+	}
+	// Defer close of http.Get call
+	defer response.Body.Close()
+
+	// Grab the entire response body.
+	// Warning! ioutil.ReadAll is not memory efficient, therefore generally not recommended.
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Error while reading response body %v\n", err)
+		return
 	}
 
-	s, resp, err := c.Messages.Send(From, To, params)
+	// Print the entire response body to terminal.
+	fmt.Println(string(contents))
+
+	// Get the TWILIO_ACCOUNTSID environment variable
+	twilioAccountSID, exists := os.LookupEnv("TWILIO_ACCOUNTSID")
+	if exists == false {
+		log.Fatalf("Error fetching environment variables: %v", exists)
+		return
+	}
+
+	// Get the TWILIO_AUTH_TOKEN
+	twilioAuthToken, exists := os.LookupEnv("TWILIO_AUTH_TOKEN")
+	if exists == false {
+		log.Fatalf("Error fetching environment variables: %v", exists)
+		return
+	}
+
+	twilioFromNum, exists := os.LookupEnv("TWILIO_FROM_NUM")
+	if exists == false {
+		log.Fatalf("Error fetching environment variables: %v", exists)
+		return
+	}
+
+	twilioToNum, exists := os.LookupEnv("TWILIO_TO_NUM")
+	if exists == false {
+		log.Fatalf("Error fetching environment variables: %v", exists)
+		return
+	}
+
+	// Initialize Twilio client
+	c := twilio.NewClient(twilioAccountSID, twilioAuthToken, nil)
+
+	// Create message params
+	params := twilio.MessageParams{
+		Body: string(contents),
+	}
+
+	// Send SMS via Twilio
+	s, resp, err := c.Messages.Send(twilioFromNum, twilioToNum, params)
 	log.Println("Send:", s)
 	log.Println("Response:", resp)
 	log.Println("Err:", err)
